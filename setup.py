9e44a5d8ab3751669ce8aa37674145ea36770264
Joseph Montoya <joseph.montoya@tri.global>
2020-04-28 14:12:16 -0700

    update notebook, setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="tri_hackathon_2020",
-    version="2020.04.28",
+    version="2020.4.28",
     packages=find_packages(),
     install_requires=[
-        "camd>=2020.3.24"
+        "camd @ git+https://github.com/TRI-AMDD/camd@dev#egg=camd"
     ],
     # TODO: make dev master
     # Enforce dev version of CAMD
-    dependency_links=[
-        "http://github.com/TRI-AMDD/camd/tarball/dev#egg=qmpy"
-    ]
+    # dependency_links=[
+    #     "git+https://github.com/TRI-AMDD/camd/tarball/dev#egg=camd"
+    # ]
 )
