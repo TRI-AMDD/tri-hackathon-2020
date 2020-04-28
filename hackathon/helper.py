5cf31ff4ac000b69866cf0667908f1e8ad87835d
Joseph Montoya <joseph.montoya@tri.global>
2020-04-28 00:45:48 -0700

    add initial hackathon helper and update notebook

---
+++ hackathon/helper.py
@@ -0,0 +1,11 @@
+# Copyright 2020 Toyota Research Institute
+
+from camd.analysis.base import Analyzer
+
+
+class SimpleAnalyzer(Analyzer):
+    def __init__(self):
+        pass
+
+    def analyze(self, new_experiment_data, seed_data):
+        pass
