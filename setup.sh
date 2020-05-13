71e474052900dda2f56488d1182d2e9b273bdb32
Joseph Montoya <joseph.montoya@tri.global>
2020-05-13 16:09:18 -0700

    update pictures, setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 echo "installing camd"
 sudo -u ec2-user -i <<'EOF'
 unset SUDO_UID
-source activate /home/ec2-user/SageMaker/custom-miniconda/envs/AMDD
-cd /home/ec2-user/SageMaker/tri-hackathon-2020
+source activate AMDD
+cd
+pip install tensorflow==1.15.2
 pip install -r requirements.txt
 pip install -e .
+
+# Install ipywidgets and widgets extensions
+pip install ipywidgets
+source activate JupyterSystemEnv  # not sure if this is necessary
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter nbextension enable --py widgetsnbextension
 EOF
+
+restart jupyter-server
